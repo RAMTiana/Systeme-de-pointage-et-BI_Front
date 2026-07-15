ef490facf2cc6b2084dab2759050ea88eb510c0f
RAMTiana <zftiana14@gmail.com>
2026-07-15 10:53:10 +0200

    ajout du clé GOOGLE_CLIENT_ID

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'http://localhost:8000/api/v1',
   // Client ID OAuth2 Google (console Google Cloud > Identifiants > ID client OAuth).
   // Doit correspondre au GOOGLE_CLIENT_ID configuré côté backend (.env).
-  googleClientId: '',
+  googleClientId: '689643947131-iucmm8a59hg4jki1e56bov5hg1uk9t76.apps.googleusercontent.com',
 };
